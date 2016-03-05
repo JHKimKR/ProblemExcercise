f9c7945659e9d9b76e73c8ffe28ae7af12a1bc66
KimJongHan <kjh929y@gmail.com>
2016-03-05 13:09:50 +0900

    Commit!

--- 20160225_1stSumPrimeNumber/main.c
+++ 20160225_1stSumPrimeNumber/main.c
@@ -1,15 +1,34 @@
 //
 //  main.c
-//  20160225_1stSumPrimeNumber
+//  20160225_1Problem
 //
-//  Created by KimJongHan on 2016. 3. 5..
+//  Created by KimJongHan on 2016. 2. 25..
 //  Copyright © 2016년 KimJongHan. All rights reserved.
 //
+/******************************************************
+ This problem is to sum up the prime numbers of a input number.
+ ******************************************************/
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    
+    int num;
+    
+    int sum = 0;
+    
+    printf("Input the number!");
+    
+    scanf("%d", &num);
+    
+    for(int idx = 1; idx < num ; idx++)
+    {
+        if( (num % idx) == 0 )
+            sum += idx;
+        else
+            continue;
+    }
+    
+    printf("%d", sum);
     return 0;
 }

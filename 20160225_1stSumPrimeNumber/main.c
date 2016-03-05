743e686b40c45da8846eebabcbf9493ec86c3a7a
KimJongHan <kjh929y@gmail.com>
2016-03-05 13:06:45 +0900

    Initial Commit

---
+++ 20160225_1stSumPrimeNumber/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  20160225_1stSumPrimeNumber
+//
+//  Created by KimJongHan on 2016. 3. 5..
+//  Copyright © 2016년 KimJongHan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
